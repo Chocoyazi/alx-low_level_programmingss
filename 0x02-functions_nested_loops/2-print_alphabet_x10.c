d724d461a2b2219fbdd11ae832ed96c384a1cb0b
chocoyazi <chocoyazi@github.com>
2023-06-20 17:34:26 -0700

    new commit

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,17 +1,14 @@
 #include "main.h"
-
 /**
- * print_alphabet_x10 - prints 10 times the alphabet in lowercase followed by e new line
- *
+ * print_alphabet_x10 - Make alphabet x10 times
+ * Return:void
  */
-
 void print_alphabet_x10(void)
 {
 	char ch;
 	int i;
 
 	i = 0;
-	
 	while (i < 100)
 	{
 		ch = 'a';

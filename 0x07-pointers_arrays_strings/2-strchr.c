735660f1f5684a0d22fa3acb5794487a0d1c2434
chocoyazi <chocoyazi@github.com>
2023-07-03 18:02:32 -0700

    new commit

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/*
+ * _strchr - Entry point
+ * @s: input
+ * @c: input
+ * Return: Always 0 (success)
+ */
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	int i = 0;
+
+	for (; s[i] >= '\0'; i++;)
+	{
+		if (s[i] == c)
+			return (&s[i]);
+	}
+	return (0);
+}
